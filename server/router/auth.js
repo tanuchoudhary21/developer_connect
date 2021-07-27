e92844f1d32372d93758c6aea2bec780d4346013
tanuchoudhary21 <chtaruna90@gmail.com>
2021-07-27 18:08:34 +0530

    Stored the user data on Atlas using express and mongoose

--- server/router/auth.js
+++ server/router/auth.js
@@ -1,13 +1,38 @@
 const express = require('express');
 const router = express.Router();
 
+require('../db/connection');
+const User = require('../model/userSchema');
+
 router.get('/', (req, res) => {
     res.send(`Welcome to home page`);
 });
 
 router.post('/register', (req, res) => {
-    console.log(req.body);
-    res.json({message: req.body})
+
+    const { name, email, phone, work, password, cpassword } = req.body;
+
+    if (!name || !email || !phone || !work || !password || !cpassword ) {
+        return res.status(422).json({error: "please fill the feild properly "});
+    }
+
+    User.findOne({email: email})
+        .then((userExist) => {
+            if (userExist) {
+                return res.status(422).json({error: "Email already Exists"});
+            }
+
+            const user = new User({name, email, phone, work, password, cpassword});
+
+            user.save().then(() => {
+                res.status(201).json({message: "User registered successfully"})
+            }).catch((err) => res.status(500).json({error: "Failed to register"}));
+
+        }).catch((err) => {console.log(err); });
+
+    // console.log(name);
+    // console.log(email);
+    // res.json({message: req.body})
     // res.send("Register Page");
 });
 
