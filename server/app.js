47a58e50611723aed7929068734fd650f7efb5f2
tanuchoudhary21 <chtaruna90@gmail.com>
2021-07-27 15:01:53 +0530

    Added Atlas connection

--- server/app.js
+++ server/app.js
@@ -1,11 +1,40 @@
+const dotenv = require("dotenv");
+const mongoose = require('mongoose');
 const express = require('express');
 
 const app = express();
+dotenv.config({path: './config.env'}); //we don't have to write it over and over again
+require('./db/connection');
+
+const PORT = process.env.PORT;
+
+const middleware = (req, res, next) => {
+    console.log("Added middleware");
+    next();
+};
+
+
 
 app.get('/' , (req, res) => {
     res.send(`Hello world`);
 });
 
-app.listen(3000, () => {
-    console.log(`Server is running on port 3000`);
+app.get('/about' ,middleware, (req, res) => {
+    res.send(`Hello world, about us page`);
+});
+
+app.get('/contact' , (req, res) => {
+    res.send(`Hello world, contact us page`);
+});
+
+app.get('/signin' , (req, res) => {
+    res.send(`Hello world, login page`);
+});
+
+app.get('/signup' , (req, res) => {
+    res.send(`Hello world, register page`);
+});
+
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
 });
