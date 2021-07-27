14205ba28d7f1dc90d71d1e6b03b09e1c8aed8de
tanuchoudhary21 <chtaruna90@gmail.com>
2021-07-27 18:32:29 +0530

    Added Data to MongoDB Atlas using async-await method and replaced previous promises method

--- server/router/auth.js
+++ server/router/auth.js
@@ -8,7 +8,7 @@ router.get('/', (req, res) => {
     res.send(`Welcome to home page`);
 });
 
-router.post('/register', (req, res) => {
+router.post('/register', async(req, res) => {
 
     const { name, email, phone, work, password, cpassword } = req.body;
 
@@ -16,24 +16,51 @@ router.post('/register', (req, res) => {
         return res.status(422).json({error: "please fill the feild properly "});
     }
 
-    User.findOne({email: email})
-        .then((userExist) => {
-            if (userExist) {
-                return res.status(422).json({error: "Email already Exists"});
-            }
+    try {
+        const userExist = await User.findOne({email: email});
 
-            const user = new User({name, email, phone, work, password, cpassword});
+        if (userExist) {
+            return res.status(422).json({error: "Email already Exists"});
+        }
 
-            user.save().then(() => {
-                res.status(201).json({message: "User registered successfully"})
-            }).catch((err) => res.status(500).json({error: "Failed to register"}));
+        const user = new User({name, email, phone, work, password, cpassword});
 
-        }).catch((err) => {console.log(err); });
+        const userResigter = user.save();
 
-    // console.log(name);
-    // console.log(email);
-    // res.json({message: req.body})
-    // res.send("Register Page");
+        if (userResigter) {
+            res.status(201).json({message: "User registered successfully"});
+        }
+
+    } catch (err) {
+        console.log(err);
+    }
+
+    
 });
+
+
+// Using promises to post data to mongoDB Atlas
+//  router.post('/register', (req, res) => {
+//     const { name, email, phone, work, password, cpassword } = req.body;
+
+//     if (!name || !email || !phone || !work || !password || !cpassword ) {
+//         return res.status(422).json({error: "please fill the feild properly "});
+//     }
+
+//     // Added data to online database using Promises
+//     User.findOne({email: email})
+//         .then((userExist) => {
+//             if (userExist) {
+//                 return res.status(422).json({error: "Email already Exists"});
+//             }
+
+//             const user = new User({name, email, phone, work, password, cpassword});
+
+//             user.save().then(() => {
+//                 res.status(201).json({message: "User registered successfully"})
+//             }).catch((err) => res.status(500).json({error: "Failed to register"}));
+
+//         }).catch((err) => {console.log(err); });
+// });
 
 module.exports = router;
