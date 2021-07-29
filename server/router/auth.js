9315dc16af2c5347f8624eef9e3dfbfdff54073e
tanuchoudhary21 <chtaruna90@gmail.com>
2021-07-29 09:49:48 +0530

    Added react to our project

--- server/router/auth.js
+++ server/router/auth.js
@@ -52,6 +52,10 @@ router.post('/signin', async(req, res) => {
             const isMatch = await bcrypt.compare(password, userLogin.password);
             const token = await userLogin.generateAuthToken();
             console.log(token);
+            res.cookie("jwtoken", token, {
+                expires: new Date(Date.now() + 25892000000),
+                httpOnly: true
+            });
             if (isMatch) {
                 return res.status(200).json({message: "You are logged in"});
             } else {
@@ -94,3 +98,4 @@ router.post('/signin', async(req, res) => {
 // });
 
 module.exports = router;
+
